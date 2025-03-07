e056ca857557cc23ae62164ec5d73592af5532ec
Nikita <reshetnyaknikita6@gmail.com>
2025-03-07 19:48:33 +0300

    chore: Install linter, set make file, add github actions

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+// main is the entry point of the application.
+// It initializes and runs the program.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nikita-reshetnyak/chat-server
+
+go 1.22.4
